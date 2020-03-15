b523f93cb867deac8fdb4b3b08fc24f22511e1a1
Natalia Mateo <nmateo7@gmail.com>
2020-03-15 15:57:34 +0100

    Fixed errors

--- js/main.js
+++ js/main.js
@@ -1,29 +1,51 @@
 "use strict";
 
-debugger;
-// general un número aleatorio entre 1 y 100
-
 const btn = document.querySelector(".js-button");
 const userNumber = document.querySelector(".js-number");
 const clues = document.querySelector(".js-clue");
+const counterNumber = document.querySelector(".js-counter");
 
+// general un número aleatorio entre 1 y 100
 function getRandomNumber(max) {
-  return Math.ceil(Math.random() * (100 - 1) + 1);
+  return Math.ceil(Math.random() * max);
+}
+// le indicas el máximo del parámetro del número
+const randomNumber = getRandomNumber(100);
+
+// suma de la funciones para llamar a las dos desde un solo listener
+function handleBtn(ev) {
+  // evitar comportamientos del form
+  ev.preventDefault();
+  getClues();
+  showCounter();
 }
 
 function getClues() {
-  debugger;
-  const userNumberValue = userNumber.value;
-  const randomNumber = getRandomNumber();
-  if (randomNumber < userNumberValue) {
-    console.log((clues.innerHTML = "Demasiado alto"));
+  // va dentro el .value porque necesitamos leerlo en este momento
+  // todo input devuelve un valor string pr eso necesitamos un parseint
+  const userNumberValue = parseInt(userNumber.value);
+  // el orden de los inputs importa, hay que poner de la que menos abarque a mas
+  if (randomNumber === userNumberValue) {
+    clues.innerHTML = "Has ganado campeona!!!";
+    // numeros menos que 100 o mayores que 1 para que el resultado sea entre 1 y 100
+  } else if (userNumberValue > 100 || userNumberValue < 1) {
+    clues.innerHTML = "El número debe estar entre 1 y 100";
+  } else if (randomNumber < userNumberValue) {
+    clues.innerHTML = "Demasiado alto";
   } else if (randomNumber > userNumberValue) {
-    console.log((clues.innerHTML = "Demasiado bajo"));
-  } else if (randomNumber === userNumberValue) {
-    console.log((clues.innerHTML = "Has ganado campeona!!!"));
+    clues.innerHTML = "Demasiado bajo";
   } else {
-    console.log((clues.innerHTML = "El número debe estar entre 1 y 100"));
+    ("Que está pasando");
   }
+  // sacar los inputs a una funcion para reutilizar código
 }
 
-btn.addEventListener("click", getClues);
+// función del contador
+/* empieza el contador en 0, una variable que sea función y que a la variable de fuera se le vaya sumando 1*/
+let counter = 0;
+const showCounter = () => {
+  counter += 1;
+  counterNumber.innerHTML = counter;
+};
+
+btn.addEventListener("click", handleBtn);
